7cb9eb22d49f02f02d29d2b8eea3878cc7c12252
Joel Santos <joeljesussantos50@gmail.com>
2024-09-05 23:58:32 +0100

    Add docstring

--- utils/passwords.py
+++ utils/passwords.py
@@ -4,7 +4,22 @@ import sys
 from .colors import change_color, DANGER
 
 
-def read_passwords(file) -> dict[str, str]:
+def read_passwords(file: str) -> dict[str, str]:
+    """
+    Reads a file in json format with the app names and passwords.
+    Returns a dict with that info
+
+    Parameters
+    ----------
+    file
+        Filename with the passwords
+
+    Returns
+    -------
+    dict[str, str]
+        Dictionary with the app name as key and the app password as
+        value
+    """
     try:
         with open(file, "r", encoding="utf-8") as f:
             return json.load(f)
@@ -14,5 +29,17 @@ def read_passwords(file) -> dict[str, str]:
 
 
 def write_passwords(file: str, passwords: dict[str, str]):
+    """
+    Writes a dictionary with the app name and passwords to a file in
+    json format
+
+    Parameters
+    ----------
+    file
+        Filename to save the passwords
+    passwords
+        Dictionary with the app name as key and the app password as
+        value
+    """
     with open(file, "w", encoding="utf-8") as f:
         json.dump(passwords, f)
